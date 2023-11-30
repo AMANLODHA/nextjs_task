d172da3c632db3b363400a9b901ab5ff03dbf283
AMANLODHA <111699707+AMANLODHA@users.noreply.github.com>
2023-11-30 16:15:46 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,16 @@
+
+async function takeTime() {
+    await new Promise ((res) => {
+        setTimeout(res, 3000);
+    });
+}
+
+export default async function About(){
+    await takeTime();
+    //throw new Error("this is manual error");
+    return (
+        <div>
+          <h1> This is about route </h1>
+        </div>
+    )
+}
